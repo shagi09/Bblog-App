4496ce6583c92e81f597c110bd0a6dbaeddf3d20
shagi09 <shalomwubu024@gmail.com>
2024-05-10 01:15:27 +0300

    changes on Header

--- ui/src/components/Header.jsx
+++ ui/src/components/Header.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>
-        Header
-      
-    </div>
+    <nav>
+      <div className='container nav_container'>
+        <Link to='/' className='nav_logo'>
+          <img src="" alt="" />
+        </Link>
+        <ul className='nav_menu'>
+          <li><Link to='/profile'>shalom wubu</Link></li>
+          <li><Link to='/create'>create post</Link></li>
+          <li><Link to='/authors'>Authors</Link></li>
+          <li><Link to='/logout'>logout</Link></li>
+        </ul>
+
+      </div>
+
+    </nav>
+
   )
 }
 
