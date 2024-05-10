fbc314616ae5a993d36b4c16949ebd66067cd3a3
shagi09 <shalomwubu024@gmail.com>
2024-05-11 02:06:56 +0300

    footer added

--- ui/src/components/Footer.jsx
+++ ui/src/components/Footer.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
     <div>
-        Footer
+        <footer>
+          <ul className='footer-link'>
+             <li><Link to='/posts/categories/:Agriculture'>Agriculture</Link></li>
+             <li><Link to='/posts/categories/:Business'>Business</Link></li>
+             <li><Link to='/posts/categories/:Education'>Education</Link></li>
+             <li><Link to='/posts/categories/:Entertainment'>Entertainment</Link></li>
+             <li><Link to='/posts/categories/:Art'>Art</Link></li>
+             <li><Link to='/posts/categories/:Investment'>Investment</Link></li>
+             <li><Link to='/posts/categories/:Uncategorized'>Uncategorized</Link></li>
+             <li><Link to='/posts/categories/:Weather'>Weather</Link></li>
+
+
+          </ul>
+
+
+        </footer>
       
     </div>
   )
