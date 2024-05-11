919731c4d3f2e0702d6f975ca3ac102f94d91406
shagi09 <shalomwubu024@gmail.com>
2024-05-11 14:34:59 +0300

    styles updated

--- ui/src/pages/ErrorPage.jsx
+++ ui/src/pages/ErrorPage.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const ErrorPage = () => {
   return (
-    <div className='error'>
-      <button className='btn-error'>Go Back Home</button>
-      <h1>page error</h1>
+
+
+      <div className='error-container'>
+        <div className='error'>
+        <Link to='/'><button className='btn-error'>Go Back Home</button></Link>
+      <h1>Page Not Found</h1>
+        </div>
+
+      
+
         
       
     </div>
