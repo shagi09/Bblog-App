1067fde457d01e156e72e50caa20bdfe37eae8aa
shagi09 <shalomwubu024@gmail.com>
2024-06-03 12:04:44 +0300

    styles updated responsiveness

--- ui/src/components/Header.jsx
+++ ui/src/components/Header.jsx
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import image from '../images/blog15.jpg'
 
 const Header = () => {
+
+
   return (
     <div>
           <nav>
@@ -10,6 +12,7 @@ const Header = () => {
         <Link to='/' className='nav_logo'>
           <img src={image} alt="logo image" />
         </Link>
+
         <ul className='nav_menu'>
           <li><Link to='/profile/:id'>shalom wubu</Link></li>
           <li><Link to='/create'>create post</Link></li>
