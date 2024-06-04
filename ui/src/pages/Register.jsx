5a9015132d2736b20d00256b87115d5229fb1ffa
shagi09 <shalomwubu024@gmail.com>
2024-06-04 19:49:11 +0300

    register updated

--- ui/src/pages/Register.jsx
+++ ui/src/pages/Register.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const Register = () => {
   const[user,setUser]=useState({
@@ -18,21 +19,33 @@ const Register = () => {
     ))
 
   }
+  function HandleSubmit(e){
+    e.preventDefault();
+    axios.post('http://localhost:3001/register',{name:user.name,email:user.email,password:user.password,confirmPassword:user.confirmPassword}).then(result=>console.log(result)).catch(error=>console.log(error))
+    
+  }
   return (
     <div className='register-container'>
       <div className='register'>
+        <form action="" onSubmit={HandleSubmit}>
         <h1>Sign Up</h1>
         <p>This is an error message</p>
         <input type="text" name='name' placeholder='Full Name'value={user.name} onChange={HandleChange} />
         <input type="email" name='email' placeholder='Email'value={user.email} onChange={HandleChange} />
         <input type="password" name='password' placeholder='Password'value={user.password} onChange={HandleChange} />
         <input type="password" name='confirmPassword' placeholder='Confirm Password'value={user.confirmPassword} onChange={HandleChange} />
+        <button type='submit' className='edit-btn'>Register</button>
+      
+      <p>Already have an account?<Link to='/login'>login</Link></p>
+        </form>
+        
+
 
 
 
       </div><br />
-      <button className='edit-btn'>Register</button>
-      <p>Already have an account?<Link to='/login'>login</Link></p>
+
+      
 
       
     </div>
